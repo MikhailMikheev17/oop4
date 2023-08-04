d64e3930b55afc8663b50b62be591de392ea6322
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-08-04 17:29:56 +0300

    add solution

---
+++ untitled/src/main/java/org/example/Apple.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Apple extends Fruit{
+
+    public Apple(float weight) {
+        super(weight);
+    }
+    public Apple() {
+        super(1.0f);
+    }
+}
---
+++ untitled/src/main/java/org/example/Orange.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Orange extends Fruit{
+
+    public Orange(float weight) {
+        super(weight);
+    }
+
+    public Orange() {
+        super(1.5f);
+    }
+
+}
